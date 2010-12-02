1fd86cdf5e48d2cfd4be60c5ebab2462814a8081
cepheid <general@cepheiddesign.com>
2010-12-02 15:22:33 -0600

    first commit

---
+++ snippets.js
@@ -0,0 +1,431 @@
+<script type="text/javascript">
+jQuery.noConflict();
+
+jQuery(document).ready(function($){
+
+   $(".container").hide();
+
+   $(".trigger").click(function(){
+   $(".container").toggle(300);
+   });
+
+});
+
+</script>
+
+
+// preloading function of images
+jQuery.preloadImages = function()
+{
+ for(var i = 0; i<arguments.length; i++)
+ {
+    jQuery("<img>").attr("src", arguments[i]);
+ }
+};
+
+// use like
+$.preloadImages("image1.gif", "/path/to/image2.png", "some/image3.jpg");
+
+
+//smooth scroll on click
+$(document).ready(function() {
+   $('.backtotop').click(function(){
+       $('html, body').animate({scrollTop:0}, 'slow');
+   });
+});
+
+
+// simple tabbed box from ul function
+    $(function() {
+    var $items = $('#popup-tabbed-list>ul>li');
+    $items.click(function() {
+    $items.removeClass('selected');
+    $(this).addClass('selected');
+
+    var index = $items.index($(this));
+    $('#popup-tabbed-list>.tab-option').hide().eq(index).show();
+    }).eq(0).click();  //eq set to zero will have the first one in the list selected by default
+    });
+
+and the html:
+         <div id="popup-tabbed-list">
+           <ul>
+           <li class="option1">Prealgebra</li>
+           <li class="option2">Beginning Algebra</li>
+           <li class="option3">Intermediate Algebra</li>
+           <li class="option4">College Algebra</li>
+           </ul>
+           
+           <div class="tab-option"><h4>Prealgebra</h4><div id="demoPlayer" class="left"></div></div>
+           <div class="tab-option"><h4>Beginning Algebra</h4>Here's the vid.</div>
+           <div class="tab-option"><h4>Intermediate Algebra</h4>Here's the vid.</div>
+           <div class="tab-option"><h4>College Algebra</h4>Here's the vid.</div>
+       </div>
+
+
+//random image loader
+$(document).ready(function(){
+    bgImageTotal=20;
+    randomNumber = Math.round(Math.random()*(bgImageTotal-1))+1;
+    imgPath=('/my/image/path/'+randomNumber+'.jpg');
+  $('h1').css('background-image', ('url("'+imgPath+'")'));
+});
+
+//display latest tweet
+$.getJSON("http://twitter.com/statuses/user_timeline/username.json?callback=?", function(data) {  
+    $("#twitter").html(data[0].text);
+});
+
+//filter list items by class  -- use 1 list to control the output displayed in another list. For this example, productfilter1 and productfilter2 lists both control productlist list. Displays "noresults" div if there are no results to display.
+    $("#productfilter1, #productfilter2").click(function(e) {
+
+   $("#productfilter1, #productfilter2").find(".active").removeClass("active");
+
+   var clicked = $(e.target);  
+    clicked.addClass("active");  
+   
+   var currentId = clicked.attr("id");
+   
+   if (currentId == "pf-all"){
+   $("#noresults").addClass("hidden");
+   $(".item").show("fast");
+   } else {                    
+   $(".item").hide("fast");
+   $("."+currentId).show("fast");
+
+       var n = $("."+currentId).length;
+       if(n == 0) {
+       $("#noresults").removeClass("hidden");
+       } else {
+       $("#noresults").addClass("hidden");
+       }    
+   }
+
+   });
+
+//string replacement function and example of use, replacing the word "compatible" with a line break and additional space
+(function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+
+$("dt").replaceText( /compatible/gi, "<br />  compatible" );
+
+
+// check if an element exists
+if ($('#id').length) {
+// do stuff
+}
+
+// check if an element is visible, if not push content over 80 pixels
+    if($("#id").is(":visible"))
+       {} else {$("#content").css({'margin-left' : '80px'});}
+
+
+//check if an element doesn't exist
+if ($('#id').length==0) {
+/* do stuff here if no visible li elements exist */
+}
+
+//search query helper and real-time echo input text
+    $("#main_search").live('keyup', function() {
+           string = $("#main_search").val();
+           temp = string.replace(/[^a-zA-Z 0-9]+/g,' ');
+           //you're only worried about matching potentially ambiguous keywords. Run a filter for each pre-existing keyword to insure people can find it.
+           var synom1 = eval("/foo|bar|baz/ig");
+           var temp1 = temp.replace(synom1, "hooray");
+           var synom2 = eval("/whatever|stupid|dumb/ig");
+           var temp2 = temp1.replace(synom2, "fish");
+           
+       $("#main_search_feedback").html(temp2);                               
+   });
+
+//form field duplication for billing/shipping address fields. When user clicks checkbox called "same", shipping fields grab the billing fields value, else clears it
+$(document).ready(function(){
+   $("input#same").click(function(){
+       if ($("input#same").is(':checked'))
+       {
+           // Checked, copy values
+           $("input#shipping-email").val($("input#email").val());
+           $("input#shipping-name").val($("input#name").val());
+           $("input#shipping-phone").val($("input#phone").val());
+       }
+       else
+       {
+           // Clear on uncheck
+           $("input#shipping-email").val("");
+           $("input#shipping-name").val("");
+           $("input#shipping-phone").val("");
+       }
+   });
+});
+
+//flowplayer video, set to autoplay on load, set to disappear and display a hidden div when video finishes playing. Also includes a list of links which can ajax-load the video source. Made to work as a popup video launcher with alternative clips available too.
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+<script type="text/javascript" src="flowplayer-3.2.4.min.js"></script>
+
+
+<style type="text/css">
+    .float-left{float:left}
+    #main-flow{height:510px;width:640px;display:none;position:relative}
+   #final-screen{background-color:#FF0;height:480px;width:640px;display:none;position:absolute}
+/*    #flow-end{position:absolute;top:0;left:0;z-index:999999;display:block;height:480px;width:640px} */
+   #player{background-color:#b7ebff;background-image:url(demovideo.jpg);height:480px;width:640px;cursor:pointer;border:0;position:relative;z-index:6}
+</style>
+
+</head><body>
+
+
+<h3><a href="#" class="launcher">Launch the window</a></h3>
+
+
+<div id="video-container">
+    <div id="main-flow" class="float-left">
+       <a id="final-screen">blargh</a>
+
+       <a
+            href="http://cachefly.thinkwell.com/public/homeschool/main_demo.flv"  
+            id="player">
+           <img src="demovideo.jpg" />
+       </a>
+       
+       <a href="#" class="close">close this</a>
+    </div>
+
+    <div id="clips" class="float-left">
+       <ul>
+       <li><a href="http://cachefly.thinkwell.com/public/homeschool/main_demo.flv">Thumbnail #1</a></li>
+       <li><a href="http://my.thinkwell.com/cf/external/?40BA867A25F13D4D">Thumbnail #2</a></li>
+       <li><a href="http://my.thinkwell.com/cf/external/?92032EF5C20A56DF">Thumbnail #3</a></li>
+       <li><a href="http://my.thinkwell.com/cf/external/?92032EF5C20A56DF">Thumbnail #4</a></li>
+       </ul>
+          
+    </div>
+
+</div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    jQuery.noConflict();
+
+    jQuery(document).ready(function($){
+
+   flowplayer("player", "../flowplayer-3.2.5.swf", {
+   clip:  {
+         autoPlay: true,
+         autoBuffering: true,
+       linkUrl: "http://www.google.com",
+       onFinish: function(){
+               this.getPlugin("play").hide(),
+               $('#player').hide();
+               $('#final-screen').show();
+           }
+       }
+   });
+
+    // playlist links in "clips" element
+    var links = document.getElementById("clips").getElementsByTagName("a");
+
+    for (var i = 0; i < links.length; i++) {
+       links[i].onclick = function() {
+
+           // play the clip specified in href- attribute with Flowplayer
+           $f().play(this.getAttribute("href", 2));
+
+           return false;
+       }
+    }
+
+    player = flowplayer();
+
+       $(".launcher").click(function() {
+           $("#main-flow").css("display","block");
+           player.play();
+       });
+       $(".close").click(function() {
+           $("#main-flow").css("display","none");
+           player.stop();
+       });
+    });
+</script>
+
+
+
+</body></html>
+
+
+
+   
+//everything needed for ad banner with variable text string defined by headline of page (with text string truncation)
+<body>
+
+<div class="main-content">
+   <h2>Math Help Videos: Beg Algebra: Finding equations Given Two Points</h2>
+</div>
+
+<a href="http://www.thinkwell.com/student/store/prealgebra" title="" id="thinkwell-ad-link"><div id="thinkwell-ad"></div></a>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+   var title = $(".main-content h2").text();
+       if(title=="") { // set default/ broken value
+       title = "mathematics";
+       } else {
+       title = title.toLowerCase();  // convert everything to lowercase
+       title = title.replace("math help videos: ","")  //strips out the "Math Help Videos: " part of the string
+       title = title.replace("series: ","")
+       var titlesplit = title.split(":"); //splits the string at the : character
+       title = titlesplit[0]; // uses the first segment of the string (defined by 0 character, 1 would be the second segment and so forth)
+       var titlelink = title.replace(/[^a-zA-Z0-9]+/g,'-'); //regex replaces any spaces, extra characters as dashes for url encoding
+       };
+
+   $("#thinkwell-ad").text(title);
+   if(title=="mathematics") {} // default/ broken value link is defined by the html <a href=""> to still work if javascript disabled
+   else {
+   $("#thinkwell-ad-link").attr("href", "http://www.thinkwell.com/student/product/"+titlelink); // sets the href attribute to string + variable defined above
+   };
+});
+</script>
+
+<style>
+#thinkwell-ad {background:#0a6aa9 url(background.png) 0 0 no-repeat;color:#FF7200;font-family:Trebuchet MS, Helvetica, Arial;font-size:17px;font-weight:700;height:165px;padding:45px 0 0 10px;text-transform:uppercase;width:200px}
+#thinkwell-ad-link {text-decoration:none}
+
+</style>
+
+</body>
+
+// Random content grabber, extracts items from randomcontent.xml by request
+   $.fn.randomContent = function(options){
+
+       var defaults = {
+       xmlPath: "randomContent.xml",
+       nodeName: "content"
+       };
+
+       var options = $.extend(defaults, options);
+       var contentArray = new Array();
+       var rc = this;
+   
+       $.get(defaults.xmlPath, {}, function(xml){
+           $(defaults.nodeName,xml).each(function(i) {
+               contentArray.push($(this).text());
+           });
+   
+           getRandom = function() {
+               var num = contentArray.length
+               var randNum = Math.floor(Math.random()*num);
+           
+               var content = "";
+               for(x in contentArray){
+                   if(x==randNum){
+                       content = contentArray[x];
+                   }
+               };
+               return content;
+           }
+       
+           rc.each(function(){
+               $(this).append(getRandom());
+           });
+       });
+   };
+
+// Testimonial word bubbles calling the random content grabber, needs randomContent javascript seen above too
+
+   var numberofQuotes = 7;
+   var xmlSource = "randomcontent.xml";
+   var randomNum1 = Math.floor(Math.random()*numberofQuotes);
+   var randomNum2 = Math.floor(Math.random()*numberofQuotes);
+       if(randomNum1 == 0){randomNum1 = randomNum1+1} else
+       if(randomNum1 == randomNum2){randomNum2 = randomNum2+1} else
+       if(randomNum2 == 0){randomNum2 = randomNum1+1};
+       if(randomNum2 > numberofQuotes){randomNum2 = randomNum2-2};
+   $('.testimonial1').randomContent({xmlPath: xmlSource, nodeName: "quote"+randomNum1});
+   $('.testauthor1').randomContent({xmlPath: xmlSource, nodeName: "author"+randomNum1});
+   $('.testimonial2').randomContent({xmlPath: xmlSource, nodeName: "quote"+randomNum2});            
+   $('.testauthor2').randomContent({xmlPath: xmlSource, nodeName: "author"+randomNum2});
+
+// simple equal height columns, applies to class of "sameheights", with padding in this case (buggy in chrome/safari browser, so first check for that browser and give a height of 380 px)
+if( $.browser.safari ){
+  $(".sameheights").height(380);
+ } else {
+
+    function equalHeight(group) {
+   var tallest = 0;
+   group.each(function() {
+       var thisHeight = $(this).height();
+       if(thisHeight > tallest) {
+           tallest = thisHeight;
+           }
+       });
+   group.height(tallest);
+    }
+
+    equalHeight($(".sameheights"));
+    }
+
+//Adds an ordered number series of classes to another class, helpful if you need individual items on a category page to be controlled individually
+
+   $('.item-with-variants').addClass(function() {
+     return 'item-' + $(this).index();
+   });
+
+//Re-orders a list of 5 divs based on their nth-child value, useful if you need to change order of items from a generated list
+    var t1 = $(".parent .div:nth-child(5)").html();
+    var t2 = $(".parent .div:nth-child(4)").html();
+    var t3 = $(".parent .div:nth-child(3)").html();
+    var t4 = $(".parent .div:nth-child(1)").html();
+    var t5 = $(".parent .div:nth-child(2)").html();
+    var reordered = t1+t2+t3+t4+t5;
+    $(".parent").html(reordered);
+
+//focus field on page load, just add "focused" class to it
+    $(".focused").focus();
+
+//everything you need to launch a flowplayer video and start/stop playback with external javascript
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+   <script type="text/javascript" src="flowplayer-3.2.4.min.js"></script>
+
+</head><body>
+
+       <a
+            href="http://cachefly.thinkwell.com/public/homeschool/main_demo.flv"  
+            style="background-image:url(demovideo.jpg);height:480px;width:640px;cursor:pointer;border:0;display:block"
+            id="player">
+           <img src="demovideo.jpg" alt=""/>
+       </a>
+   
+       <script>
+           flowplayer("player", "../flowplayer-3.2.5.swf", {
+           clip:  {
+               autoPlay: false,
+               autoBuffering: true
+            }
+       });
+        // this is the key line, defining the var player to be your flowplayer object
+       player = flowplayer();
+
+       </script>
+   
+<button onClick="player.pause();">pause</button>
+<button onClick="player.play();">play</button>
+
+
+</body></html>
+
+// extract portion of url string, in this case the last part (whatever) of http://www.example.com/products/whatever, set this value to webid
+url = window.location.href;
+var webid = url.substring(url.lastIndexOf('/') + 1);
+
+// jquery code insertion format
+before<div>prepend (text/html) append</div>after
+
+for troubleshooting, show what jquery thinks the element ID is when clicked
+    $(".whatever").click(function() {
+       var thistrigger = $(this).attr("id");
+       alert(thistrigger);
+       });
